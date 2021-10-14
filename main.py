443b33f158bc6e55cd46b355351a8844333f949b
andi <andi.muehlmann@arcor.de>
2021-10-14 20:07:55 +0200

    before pull

--- main.py
+++ main.py
@@ -9,57 +9,26 @@ def get_weigts():
     weights = []
     for i in range(1, len(doc), 2):
         weights.append([int(doc[i]), int(doc[i + 1])])
-    return weights
+    return weights   
 
-def give_shorter_combination(combination1, combination2):
-    if len(combination1) < len(combination2):
-        return combination1
-    else:
-        return combination2
-
-def give_possible_combination(combination1, combination2):
-    if combination1 and combination2:
-            return give_shorter_combination(combination1, combination2)
-    elif combination1:
-        return combination1 
-    elif combination2:
-        return combination2
-    else:
-        return False        
-
-def call_find_combination_with_the_right_values(weights, searched_weight, weight, combination):
-    weights[weight][1] -= 1
-    combination1 = find_combination(weights, searched_weight + weights[weight][0], combination + deque([weights[weight][0]]))
-    combination2 = find_combination(weights, searched_weight - weights[weight][0], combination + deque([-weights[weight][0]]))
-    return combination1, combination2
-
-def find_combination(weights, searched_weight, combination=deque([]), deleted_weights=0):
-    if searched_weight == 0:
-        return combination
-    for weight in range(len(weights)): #give_best weights in order. For that make funktion. 
-        weight -= deleted_weights
-        weights = copy.deepcopy(weights)
-        if weights[weight][1] == 0:
-            del weights[weight]
-            deleted_weights += 1
-            continue 
-        combination1, combination2 = call_find_combination_with_the_right_values(weights, searched_weight, weight, combination)
-        combination = give_possible_combination(combination1, combination2)
-        if combination:
-            return combination
-    return False
+def find_combination(weights, searched_weight):
+    closesed_weight = None
+    return
 
 def print_combination_for_weight(weight, combination):
-    pass
-
+    print(f'weight {weight}:')
+    for step in combination:
+        print(step)
+    
 def main():
     #weights = get_weigts()
-    weights = [10, 5]
-    count_weights = [1, 3]
+    weights = [
+        [10, 1], [5, 3]
+        ]
     for weight in range(10,20, 10):
         combination = find_combination(weights, weight)
         if combination:
-            print_combination_for_weight(weight, combination)
+            print_combination_for_weight(weight[0], combination)
         else:
             print(f'There is no combination for {weight}g')
     return
