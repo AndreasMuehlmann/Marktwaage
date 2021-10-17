463d5ec3c6b535c252f908854e8da47596aed20b
andi <andi.muehlmann@arcor.de>
2021-10-17 14:36:27 +0200

    works but to slow

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 import requests
 from collections import deque
+import copy
 
 class Node:
 
-    def __init__(self, weight, total_weight, previous):
-        self.id = id 
+    def __init__(self, availabel_weights, weight, total_weight, previous):
+        self.id = id(self)
         self.previous = previous
         self.weight = weight
         self.total_weight = total_weight
-    
-    def give_unused_id():
-        id = 0
-        while True:
-            id += 1
-            yield id 
-            
+        self.available_weights = availabel_weights
+
+    def  give_available_weights(self):
+        for weight in self.available_weights:
+            if weight[0] == self.weight:
+                weight[1] -= 1
+            if weight[1] == 0:
+                del weight
+        return self.available_weights
+
 def get_weigts():
-    url = 'https://bwinf.de/fileadmin/user_upload/gewichtsstuecke0.txt'
+    url = 'https://bwinf.de/fileadmin/user_upload/gewichtsstuecke1.txt'
     result = requests.get(url)
     doc = result.content.decode("utf-8").split()
     weights = []
@@ -25,48 +29,52 @@ def get_weigts():
     return weights   
 
 def get_closesed_weight(searched_weight, visited):
-    closesed_weight = 0
-    for weight in visited:
-        if abs(closesed_weight - searched_weight) < abs(weight.value - searched_weight):
-            closesed_weight = weight
+    closesed_weight = visited[0]
+    for node in visited:
+        if abs(closesed_weight.total_weight - searched_weight) < abs(node.weight - searched_weight):
+            closesed_weight = node
     return closesed_weight
 
 def get_path(end, visited, path=[]):
-    path.appendleft(end)
+    path = [end] + path
     for node in visited:
         if node.id == end.previous:
-            path.appendleft(node.weight)
-            return get_path(0, node, path)
+            return get_path(node, visited, path)
     return path
+
+def  give_available_weights(weights):
+    for weight in weights:
+        weight[1] -= 1
+        if weight[1] == 0:
+            del weight
+    return weights
 
 def find_combination(weights, searched_weight):
     visited = []
-    queue = deque([Node(0, True, None)])
-    while len(queue) != 0:
-        for node in queue:
-            if node.weight == searched_weight:
-                return get_path(node, visited)
-            for weight in weights:
-                if weight[1] == 0:
-                    continue
-                queue.append(Node(node.weight + weight[0], True))            
-                queue.append(Node(node.weight - weight[0], False))
-            visited.append(queue.popleft())
-    closesed_weight = get_closesed_weight(visited, searched_weight)
-    return get_path(closesed_weight, visited)
+    queue = deque([Node(weights, 0, 0, None)])
+    while queue:
+        visited.append(queue[0])
+        node = queue.popleft()
+        weights = node.give_available_weights()
+        if node.total_weight == searched_weight:
+            return get_path(node, visited)
+        for weight in weights: 
+            queue.append(Node(node.available_weights, +weight[0], node.total_weight + weight[0], node.id)) 
+            if node.total_weight > searched_weight:           
+                queue.append(Node(node.available_weights, -weight[0], node.total_weight - weight[0], node.id))
+    return get_path(get_closesed_weight(searched_weight, visited), visited)
 
-def print_path_for_weight(weight, path):
+def print_path_for_weight(weight , path):
     print(f'weight {weight}:')
-    for step in path:
-        print(step)
+    for node in path:
+        print(node.weight)
 
 def main():
-    #weights = get_weigts()
-    weights = [[10, 1], [5, 3]]
-    for weight in range(10,20, 10):
-        path = find_combination(weights, weight)
+    weights = get_weigts()
+    for weight in range(10, 1000, 10):
+        path = find_combination(copy.deepcopy(weights), weight)
         if path:
-            print_path_for_weight(weight, path)
+            print_path_for_weight(weight , path)
         else:
             print(f'There is no combination for {weight}g')
     return
