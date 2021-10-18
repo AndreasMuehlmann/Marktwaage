6a99b4cbe081efe106b7098031478e9a4f813803
andi <andi.muehlmann@arcor.de>
2021-10-18 20:29:40 +0200

    slow but working

--- main.py
+++ main.py
@@ -4,23 +4,26 @@ import copy
 
 class Node:
 
-    def __init__(self, availabel_weights, weight, total_weight, previous):
+    def __init__(self, weight, total_weight, previous):
         self.id = id(self)
         self.previous = previous
         self.weight = weight
         self.total_weight = total_weight
-        self.available_weights = availabel_weights
 
-    def  give_available_weights(self):
-        for weight in self.available_weights:
-            if weight[0] == self.weight:
-                weight[1] -= 1
-            if weight[1] == 0:
-                del weight
-        return self.available_weights
+    def  give_available_weights(self, weights, path):
+        deleted_weights = 0
+        for weight in path:
+            for index in range(len(weights)):
+                index -= deleted_weights
+                if weight == weights[index][0]:
+                    weights[index][1] -= 1
+                    if weights[index][1] == 0:
+                        del weights[index] 
+                        deleted_weights += 1
+        return weights 
 
 def get_weigts():
-    url = 'https://bwinf.de/fileadmin/user_upload/gewichtsstuecke1.txt'
+    url = 'https://bwinf.de/fileadmin/user_upload/gewichtsstuecke0.txt'
     result = requests.get(url)
     doc = result.content.decode("utf-8").split()
     weights = []
@@ -36,42 +39,47 @@ def get_closesed_weight(searched_weight, visited):
     return closesed_weight
 
 def get_path(end, visited, path=[]):
-    path = [end] + path
+    path = [end.weight] + path
     for node in visited:
         if node.id == end.previous:
             return get_path(node, visited, path)
     return path
 
-def  give_available_weights(weights):
-    for weight in weights:
-        weight[1] -= 1
-        if weight[1] == 0:
-            del weight
-    return weights
+def check_if_similar_path_already_there(path, visited):
+    for node in visited:
+        if node.total_weight == sum(path):
+            are_similar = set(get_path(node, visited)) == set(path)
+            if are_similar: 
+                return True
+    return False
 
-def find_combination(weights, searched_weight):
+def find_combination(start_weights, searched_weight):
     visited = []
-    queue = deque([Node(weights, 0, 0, None)])
+    queue = deque([Node(0, 0, None)])
     while queue:
-        visited.append(queue[0])
-        node = queue.popleft()
-        weights = node.give_available_weights()
+        node = queue[0]
+        path = get_path(node, visited)
+        if check_if_similar_path_already_there(path, visited):
+            del queue[0]
+            continue
+        weights = node.give_available_weights(copy.deepcopy(start_weights), path)
         if node.total_weight == searched_weight:
-            return get_path(node, visited)
+            return path
         for weight in weights: 
-            queue.append(Node(node.available_weights, +weight[0], node.total_weight + weight[0], node.id)) 
+            queue.append(Node(+weight[0], node.total_weight + weight[0], node.id)) 
             if node.total_weight > searched_weight:           
-                queue.append(Node(node.available_weights, -weight[0], node.total_weight - weight[0], node.id))
+                queue.append(Node(-weight[0], node.total_weight - weight[0], node.id))
+        visited.append(queue.popleft())
     return get_path(get_closesed_weight(searched_weight, visited), visited)
 
 def print_path_for_weight(weight , path):
     print(f'weight {weight}:')
-    for node in path:
-        print(node.weight)
+    for weight in path:
+        print(weight)
 
 def main():
     weights = get_weigts()
-    for weight in range(10, 1000, 10):
+    for weight in range(10, 10000, 10):
         path = find_combination(copy.deepcopy(weights), weight)
         if path:
             print_path_for_weight(weight , path)
