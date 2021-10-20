2245c2f9702042feee337ff8c1fe07b211818c73
andi <andi.muehlmann@arcor.de>
2021-10-20 16:39:32 +0200

    faster if weight is reachable

--- main.py
+++ main.py
@@ -1,28 +1,17 @@
+from __future__ import annotations
 import time
 import requests
-from collections import deque
 import copy
+from dataclasses import dataclass
 
+
+@dataclass(order=True)
 class Node:
-
-    index = None
-
-    def __init__(self, weight, total_weight, previous):
-        self.previous = previous
-        self.weight = weight
-        self.total_weight = total_weight
-
-    def  give_available_weights(self, weights, path):
-        deleted_weights = 0
-        for node in path:
-            for index in range(len(weights)):
-                index -= deleted_weights
-                if node.weight == weights[index][0]:
-                    weights[index][1] -= 1
-                    if weights[index][1] == 0:
-                        del weights[index] 
-                        deleted_weights += 1
-        return weights 
+    available_weights : list
+    weight : int 
+    total_weight : int
+    previous : Node
+    index : int = None
 
 def get_weigts():
     url = 'https://bwinf.de/fileadmin/user_upload/gewichtsstuecke0.txt'
@@ -33,7 +22,10 @@ def get_weigts():
         start_weights.append([int(doc[i]), int(doc[i + 1])]) 
     return start_weights
 
-def get_closesed_weight(searched_weight, visited):
+def sort_helper(node):
+    return abs(searched_weight - node.total_weight)
+
+def get_closesed_weight(visited):
     closesed_weight = visited[0]
     for node in visited:
         if abs(closesed_weight.total_weight - searched_weight) < abs(node.weight - searched_weight):
@@ -46,15 +38,25 @@ def get_path(end, visited, path=[]):
         return path
     return get_path(visited[end.previous], visited, path)
 
-def check_if_similar_path_already_there(path, visited):
-    path = set(path)
+def path_exists(path, visited): # dont use
     for node in visited:
-        are_similar = set(get_path(node, visited)) == path
+        are_similar = sorted(get_path(node, visited)) == sorted(path)
         if are_similar: 
             return True
     return False
 
-def reachable(weights, searched_weight, current_weight):
+def remove_all_matching_nodes(visited): # dont use
+    deleted = 0
+    for node_index, node in  enumerate(visited):
+        node_index -= deleted
+        if path_exists(get_path(node, visited), visited):
+            del visited[node_index] 
+            deleted += 1
+        node.index -= deleted
+        node.previous
+    return visited
+
+def reachable(weights, current_weight): #dont use
     total = 0
     for weight in weights: 
         for _ in range(weight[1]):
@@ -63,43 +65,45 @@ def reachable(weights, searched_weight, current_weight):
         return False
     return True
 
-def find_combination(weights, searched_weight):
+def remove_one_weight(weights, index):
+    weights[index][1] -= 1
+    if weights[index][1] == 0:
+        del weights[index]
+    return weights
+
+def find_combination(weights):
     visited = []
-    queue = deque([Node(0, 0, None)])
+    queue = [Node(weights, 0, 0, None)]
     while queue:
         node = queue[0]
         node.index = len(visited)
-        path = get_path(node, visited)
         if node.total_weight == searched_weight:
-            return path
-        #if check_if_similar_path_already_there(path, visited):
-        #    del queue[0]
-        #    continue
-        available_weights = node.give_available_weights(copy.deepcopy(weights), path)
-        #if not reachable(weights, searched_weight, node.total_weight): 
-        #    continue
-        for weight in available_weights: 
+            return get_path(node, visited)
+        for index, weight in enumerate(node.available_weights): 
             if node.total_weight < searched_weight:           
-                queue.append(Node(+weight[0], node.total_weight + weight[0], node.index)) 
+                queue.append(Node(remove_one_weight(copy.deepcopy(node.available_weights), index), +weight[0], node.total_weight + weight[0], node.index)) 
             else:
-                queue.append(Node(-weight[0], node.total_weight - weight[0], node.index))
-        visited.append(queue.popleft())
-    return get_path(get_closesed_weight(searched_weight, visited), visited)
+                queue.append(Node(remove_one_weight(copy.deepcopy(node.available_weights), index), -weight[0], node.total_weight - weight[0], node.index))
+        visited.append(queue[0])
+        del queue[0]
+        queue.sort(key=sort_helper)
+    return get_path(get_closesed_weight(visited), visited)
 
-def print_path_for_weight(weight , path):
-    print(f'weight {weight}:')
+def print_path_for_weight(path):
+    print(f'weight {searched_weight}:')
     for node in path:
         print(node.weight)
 
 def main():
+    global searched_weight
     weights = get_weigts()
-    start = time.time()
-    for start_weigths in range(10, 1000, 10):
-        path = find_combination(copy.deepcopy(weights), start_weigths)
+    start = time.time() 
+    for searched_weight in range(10, 1000, 10):
+        path = find_combination(copy.deepcopy(weights))
         if path:
-            print_path_for_weight(start_weigths , path)
+            print_path_for_weight(path)
         else:
-            print(f'There is no combination for {start_weigths}g')
+            print(f'There is no combination for {searched_weight}g')
     print(f'time: {time.time() - start}')
     return
 
