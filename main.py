dbefb002437e7a4e28ee8eedc55bb787f3ce6725
andi <andi.muehlmann@arcor.de>
2021-11-10 20:37:26 +0100

    no globals

--- main.py
+++ main.py
@@ -25,12 +25,13 @@ def get_weigts(url):
         start_weights.append((int(doc[i]), int(doc[i + 1]))) 
     return start_weights
 
-def get_closesed_weight(searched_weight, index_previous_closesed_weight):
+def get_closesed_weight(visited, searched_weight, index_previous_closesed_weight):
     closesed_weight = visited[0]
     for node_index in range(index_previous_closesed_weight, len(visited)):
         if abs(closesed_weight.total_weight - searched_weight) > abs(visited[node_index].total_weight - searched_weight):
             closesed_weight = visited[node_index]
             index_previous_closesed_weight = node_index 
+
         elif visited[node_index].total_weight > closesed_weight.total_weight:
             return closesed_weight, index_previous_closesed_weight
     return closesed_weight, index_previous_closesed_weight
@@ -42,6 +43,7 @@ def get_path(end, path=[]):
     return get_path(end.previous, path)
 
 def get_combinations(weights):
+    visited = [Node(0, 0, 0)]
     for weight in weights:
         visited_add_on = []
         for node in visited:
@@ -49,8 +51,9 @@ def get_combinations(weights):
                     visited_add_on.append(Node(count + 1, +weight[0], node.total_weight, node)) 
                     visited_add_on.append(Node(count + 1, -weight[0], node.total_weight, node))
         visited.extend(visited_add_on)
+    return visited
 
-def get_index_first_0():
+def get_index_first_0(visited):
     index = len(visited) // 2
     while index > 0 and visited[index].total_weight >= 0:
         index -= 1
@@ -68,21 +71,20 @@ def write_path_for_weight_to_file(path, searched_weight, file):
 def main():
     global visited
     for task in range(6):
-        visited = [Node(0, 0, 0)]
         with open(f'marktwaage_results{task}.txt', 'w') as file:
             file.write('Marktwaage\n\n')
             file.write(f'Test {task}\n\n')
             weights = get_weigts(f'https://bwinf.de/fileadmin/user_upload/gewichtsstuecke{task}.txt')
 
             start = time.time() 
-            get_combinations(weights)
+            visited = get_combinations(weights)
             print(f'time get_combinations: {time.time() - start}')
 
             visited.sort(key = lambda node : node.total_weight)
-            index_previous_closesed_weight = get_index_first_0()
+            index_previous_closesed_weight = get_index_first_0(visited)
 
             for searched_weight in range(10, 10010, 10):
-                closesed_weight, index_previous_closesed_weight = get_closesed_weight(searched_weight, index_previous_closesed_weight)
+                closesed_weight, index_previous_closesed_weight = get_closesed_weight(visited, searched_weight, index_previous_closesed_weight)
                 path = get_path(closesed_weight)
                 write_path_for_weight_to_file(path, searched_weight, file) 
 
