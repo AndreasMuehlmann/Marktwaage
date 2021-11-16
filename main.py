8d5cdd217d582a95c3f8aa4828ce3eaa028a10d6
andi <andi.muehlmann@arcor.de>
2021-11-16 13:12:26 +0100

    probably final commit

--- main.py
+++ main.py
@@ -25,16 +25,17 @@ def get_weigts(url):
         start_weights.append((int(doc[i]), int(doc[i + 1]))) 
     return start_weights
 
-def get_closesed_weight(visited, searched_weight, index_previous_closesed_weight):
+def get_closesed_weight(visited, searched_weight, search_start):
     closesed_weight = visited[0]
-    for node_index in range(index_previous_closesed_weight, len(visited)):
+    for node_index in range(search_start, len(visited)):
         if abs(closesed_weight.total_weight - searched_weight) > abs(visited[node_index].total_weight - searched_weight):
             closesed_weight = visited[node_index]
-            index_previous_closesed_weight = node_index 
+            search_start = node_index 
 
         elif visited[node_index].total_weight > closesed_weight.total_weight:
-            return closesed_weight, index_previous_closesed_weight
-    return closesed_weight, index_previous_closesed_weight
+            return closesed_weight, search_start
+
+    return closesed_weight, search_start
 
 def get_path(end, path=[]):
     path = [end] + path
@@ -53,12 +54,6 @@ def get_combinations(weights):
         visited.extend(visited_add_on)
     return visited
 
-def get_index_first_0(visited):
-    index = len(visited) // 2
-    while index > 0 and visited[index].total_weight >= 0:
-        index -= 1
-    return index + 1
-
 def write_path_for_weight_to_file(path, searched_weight, file):
     file.write(f'weight {searched_weight} -> reached weight {path[-1].total_weight}')
     for node in path:
@@ -69,7 +64,6 @@ def write_path_for_weight_to_file(path, searched_weight, file):
     return
 
 def main():
-    global visited
     for task in range(6):
         with open(f'marktwaage_results{task}.txt', 'w') as file:
             file.write('Marktwaage\n\n')
@@ -81,10 +75,10 @@ def main():
             print(f'time get_combinations: {time.time() - start}')
 
             visited.sort(key = lambda node : node.total_weight)
-            index_previous_closesed_weight = get_index_first_0(visited)
+            search_start = len(visited) // 2 # index of first 0
 
             for searched_weight in range(10, 10010, 10):
-                closesed_weight, index_previous_closesed_weight = get_closesed_weight(visited, searched_weight, index_previous_closesed_weight)
+                closesed_weight, search_start = get_closesed_weight(visited, searched_weight, search_start)
                 path = get_path(closesed_weight)
                 write_path_for_weight_to_file(path, searched_weight, file) 
 
